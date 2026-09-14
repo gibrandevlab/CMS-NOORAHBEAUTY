7ea11897a39119ec6b4112c5c273f1a8a9bb6345
gibrandevlab <afwangibran19@gmail.com>
2026-09-14 09:44:04 +0700

    update icon dan perubahan halaman berita

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,50 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { addIcons } from 'ionicons';
+import {
+  addOutline,
+  alertCircleOutline,
+  briefcaseOutline,
+  businessOutline,
+  calendarOutline,
+  checkmarkCircleOutline,
+  chevronBackOutline,
+  chevronForwardOutline,
+  closeOutline,
+  createOutline,
+  eyeOffOutline,
+  eyeOutline,
+  folderOpenOutline,
+  informationCircleOutline,
+  newspaperOutline,
+  pricetagsOutline,
+  refreshOutline,
+  searchOutline,
+  trashOutline,
+} from 'ionicons/icons';
 
 import { AppModule } from './app/app.module';
+
+addIcons({
+  addOutline,
+  alertCircleOutline,
+  briefcaseOutline,
+  businessOutline,
+  calendarOutline,
+  checkmarkCircleOutline,
+  chevronBackOutline,
+  chevronForwardOutline,
+  closeOutline,
+  createOutline,
+  eyeOffOutline,
+  eyeOutline,
+  folderOpenOutline,
+  informationCircleOutline,
+  newspaperOutline,
+  pricetagsOutline,
+  refreshOutline,
+  searchOutline,
+  trashOutline,
+});
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
